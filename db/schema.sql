fdb37488cac497f81759fcbfa3bb4c91975513da
Elmer Herrera <dcshoeco109790@yahoo.com>
2023-03-01 12:46:21 -0500

    schema table created

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE employees (
+    firtsName VARCHAR(30) NOT NULL,
+    lastName VARCHAR(30) NOT NULL,
+    title VARCHAR(30)  NOT NULL,
+    department VARCHAR(45) NOT NULL,
+    salary INT NOT NULL,
+    isManager BOOLEAN DEFAULT false,
+    manager VARCHAR(55) NOT NULL
+);
